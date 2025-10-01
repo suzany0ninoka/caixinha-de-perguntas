efe3591b304b42243935ec755c9ef212f95a556a
suzany0ninoka <dias.suzany@escola.pr.gov.br>
2025-10-01 10:34:32 -0300

    yh

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ function CriarCartão(categoria, pergunta, resposta) {
     cartao.className = 'cartao'
     cartao.innerHTML = `
         <div class="cartão_conteudo">
-            <h3>Gato de botas</h3>
+            <h3>$(categoria)</h3>
             <div class="cartão_conteudo_pergunta">
-                <p>Onde Gato de Botas apareceu pela primeira vez?</p>
+                <p>$(pergunta)</p>
             </div>
             <div class="cartão_conteudo_resposta">
-                <p> Ele fez sua primeira aparição no filme "Shrek 2", de 2004, como um caçador de ogros contratado pelo Rei Harold para capturar Shrek.</p>
+                <p>$(resposta)</p>
             </div>
         </div>
 `
