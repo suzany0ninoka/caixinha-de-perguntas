7e9cd75d44f010aa6d270b78e0040cc91191bd5c
suzany0ninoka <dias.suzany@escola.pr.gov.br>
2025-11-25 19:49:31 -0300

    ok

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
-function CriarCartão(categoria,pergunta,resposta) {
-    let container = document.getElementById('container')
-    let cartao = document.createElement('article')
-    cartao.className = 'cartao'
+function CriarCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
 
     cartao.innerHTML = `
-        <div class="cartão_conteudo">
-            <h3>$(categoria)</h3>
-            <div class="cartão_conteudo_pergunta">
-                <p>$(pergunta)</p>
+        <div class="cartao_conteudo">
+            <h3>${categoria}</h3>
+            <div class="cartao_conteudo_pergunta">
+                <p>${pergunta}</p>
             </div>
-            <div class="cartão_conteudo_resposta">
-                <p>$(resposta)</p>
+            <div class="cartao_conteudo_resposta">
+                <p>${resposta}</p>
             </div>
         </div>
-`
-    container.appendChild(cartao)
+    `;
 
+    container.appendChild(cartao);
 
-    let respostaEstaVisivel = false
-    function viraCartao() {
-        respostaEstaVisivel = !respostaEstaVisivel
-        cartao.classList.toggle('ative', respostaEstaVisivel)
-    }
-    cartao.addEventListener('click', viraCartao)
-
+    cartao.addEventListener('click', () => {
+        cartao.classList.toggle('virado');
+    });
 }
